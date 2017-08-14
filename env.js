1d7ccc8a6d4ecbefeaf46eb11ef4f2167544ed2b
Lau Skeeter <lawr@lauragonzalez.cc>
2017-08-14 12:30:18 +0200

    Initial

---
+++ env.js
@@ -0,0 +1,13 @@
+module.exports = {
+	twitterConsumerKey: process.env.MC_TWITTER_APPK,
+	twitterConsumerSecret: process.env.MC_TWITTER_APPS,
+	twitterAccess: process.env.MC_TWITTER_AT,
+	twitterSecret: process.env.MC_TWITTER_S,
+};
+
+/*
+	You can get all the Twitter keys by
+	creating an app on https://apps.twitter.com
+	Create it from your bot's account and use its own
+	access token and secret key to bypass logging in.
+*/
