a3ff5eaa10799faf78c830afd2dd21a9ddab2d42
Lau Skeeter <lawr@lauragonzalez.cc>
2017-08-14 13:09:45 +0200

    add more sources, support questiony clickbait

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 const feed = require("feed-read");
+const toTitleCase = require('to-title-case');
 
-const fetch = edition => {
+const random = (items) => items[Math.floor(Math.random()*items.length)];
+
+const replacers = [
+    `$1, and That’s a Big Opportunity for Brands`
+];
+const questionReplacers = [
+    `$1 And Why That’s a Big Opportunity for Brands`
+]
+const sources = [
+    'http://rss.cnn.com/rss/edition.rss',
+    'http://www.vice.com/en_us/rss',
+    'https://www.vox.com/rss/index.xml',
+    'http://waypoint.vice.com/en_us/rss'
+]
+
+const fetch = () => {
+
+    let edition = random(sources);
 
     return new Promise((cool,uncool)=>{
 
         feed(edition, (err, articles) => {
 
             if (err) uncool(err);
-
             let headlines = [];
 
             articles.map(article => {
-                headlines.push(`${article.title}, and That’s a Big Opportunity for Brands ${article.link}`);
+                let replacer = random(article.title.indexOf('?') > 1?questionReplacers:replacers);
+
+                console.log();
+
+                headlines.push(
+                    replacer.replace('$1',toTitleCase(article.title))
+                    + ' '
+                    + article.link
+                );
             })
 
             cool(headlines);
