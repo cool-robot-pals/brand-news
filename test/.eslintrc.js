b5d74e18bdbce9a1f5ec1b803da9ef7dabf3db4f
Lau Skeeter <lawr@lauragonzalez.cc>
2017-08-14 13:40:37 +0200

    refactor & testing

---
+++ test/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends" : "../.eslintrc.js",
+    "env"     : {
+        "mocha" : true
+    },
+    "globals" : {
+        "expect" : false,
+        "should" : false,
+        "sinon"  : false
+    }
+};
